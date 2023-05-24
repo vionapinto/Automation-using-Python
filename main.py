ef284fdfc2e77bda6476354dd97dca52f9867d43
Viona <vionapinto@gmail.com>
2023-05-24 14:19:04 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service('/home/dci-student/Downloads/chromedriver.exe')
+
+def get_drvier():
+  # Set options to make browsing easier
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+
+  driver = webdriver.Chrome(service=service, options=options)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
+
+def main():
+  driver = get_drvier()
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+  return element.text
+
+print(main())
