2c986cc7f98e2922bfc1668f91efb04a8e685c99
Viona <vionapinto@gmail.com>
2023-06-12 21:37:47 +0200

    Generating PDFs completed

--- 5_Generating_PDFs_Extracting_data_from_PDFs/generating_PDF_from_excel_spreadsheets/main.py
+++ 5_Generating_PDFs_Extracting_data_from_PDFs/generating_PDF_from_excel_spreadsheets/main.py
@@ -1,22 +1,22 @@
 import pandas
-from fpdf import FPDF
+#from fpdf import FPDF
 
 df = pandas.read_excel('data.xlsx')
-# print(df)
+print(df)
 
-for index, row in df.iterrows():
-  pdf = FPDF(orientation='P', unit='pt', format='A4')
-  pdf.add_page()
+# for index, row in df.iterrows():
+#   pdf = FPDF(orientation='P', unit='pt', format='A4')
+#   pdf.add_page()
 
-  pdf.set_font(family='Times', style='B', size=24)
-  pdf.cell(w=0, h=50, txt=row['name'], align='C', ln=1)
+#   pdf.set_font(family='Times', style='B', size=24)
+#   pdf.cell(w=0, h=50, txt=row['name'], align='C', ln=1)
 
-  for column in df.columns[1:]:
-    pdf.set_font(family='Times', style='B', size=14)
-    pdf.cell(w=100, h=25, txt=f"{column.title()}:")
+#   for column in df.columns[1:]:
+#     pdf.set_font(family='Times', style='B', size=14)
+#     pdf.cell(w=100, h=25, txt=f"{column.title()}:")
 
-    pdf.set_font(family='Times', size=14)
-    pdf.cell(w=100, h=25, txt=row[column], ln=1)
+#     pdf.set_font(family='Times', size=14)
+#     pdf.cell(w=100, h=25, txt=row[column], ln=1)
 
 
-  pdf.output(f"{row['name']}.pdf")
+#   pdf.output(f"{row['name']}.pdf")
